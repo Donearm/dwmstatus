488867d0af48f346fc8b1ac34ce637e7d2c6d257
Gianluca Fiore <forod.g@gmail.com>
2013-02-13 13:35:15 +0100

    customized config.h for my needs

--- config.h
+++ config.h
@@ -5,12 +5,12 @@
  ************************/
 
 /* Specify your timezone: */
-#define TIMEZONE "America/Chicago"
+#define TIMEZONE "Europe/Rome"
 
 /* Specify network device, usually wlan0 or eth0
  * systemd has some new naming conventions you can turn on with longer names.
  */
-#define NET_DEVICE_PRIMARY "wlp3s0"
+#define NET_DEVICE_PRIMARY "eth0"
 /* Currently not of any use 
 #define NET_DEVICE_SECONDARY "enp0s25" */
 
@@ -18,5 +18,5 @@
 #define BATT_PATH "/sys/class/power_supply/BAT0"
 
 /* Specify your temperature sensor information. */
-#define TEMP_SENSOR_PATH "/sys/devices/platform/coretemp.0"
-#define TEMP_SENSOR_UNIT "temp1_input"
+#define TEMP_SENSOR_PATH "/sys/class/hwmon/hwmon0/device"
+#define TEMP_SENSOR_UNIT "temp3_input"
